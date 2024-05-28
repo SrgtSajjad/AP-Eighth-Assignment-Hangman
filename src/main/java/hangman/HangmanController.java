407daf38b886d84fd8fa85a22ece829e008fa9fd
SrgtSajjad <msajjad.za135@gmail.com>
2024-05-29 01:49:08 +0330

    Create initial page

--- src/main/java/hangman/HangmanController.java
+++ src/main/java/hangman/HangmanController.java
@@ -1,14 +1,59 @@
 package hangman;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class HangmanController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Hyperlink logInLink;
+
+    @FXML
+    private Label logLabel;
+
+    @FXML
+    private Label menuName;
+
+    @FXML
+    private AnchorPane menuPane;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Hyperlink signUpLink;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    void changeMenuToLogIn(ActionEvent event) {
+
     }
+
+    @FXML
+    void changeMenuToSignUp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmInputs(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getLogInPage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getSignUpPage(ActionEvent event) {
+
+    }
+
 }
